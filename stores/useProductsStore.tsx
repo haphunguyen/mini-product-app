3797e0862bed6b272c4de3234cbdd86e2fc69af1
Nguyen Ha <nguyenhp@dgroup.co>
2025-06-03 11:39:30 +0700

    feature: add local caching by asyns storage

--- stores/useProductsStore.tsx
+++ stores/useProductsStore.tsx
@@ -1,4 +1,6 @@
-import { create } from 'zustand'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 export interface Product {
     id: number
@@ -27,36 +29,41 @@ type ProductStore = {
     updateQuantityProductSelected: (productId: number, quantity: number) => void
 }
 
-const useProductsStore = create<ProductStore>()((set) => ({
-    products: [],
-    setProducts: (products) => set(() => ({ products })),
+const useProductsStore = create<ProductStore>()(persist(
+    (set) => ({
+        products: [],
+        setProducts: (products) => set(() => ({ products })),
 
-    productsSelected: [],
-    addProductToCart: (product) => set((state) => {
-        const newList = [...state.productsSelected]
-        const indexItem = newList.findIndex(p => p.id === product.id)
-        if (indexItem >= 0) {
-            newList[indexItem].quantity += 1
-        } else {
-            newList.push({ id: product.id, quantity: 1 })
-        }
-        return {
-            ...state,
-            productsSelected: newList,
-        }
+        productsSelected: [],
+        addProductToCart: (product) => set((state) => {
+            const newList = [...state.productsSelected]
+            const indexItem = newList.findIndex(p => p.id === product.id)
+            if (indexItem >= 0) {
+                newList[indexItem].quantity += 1
+            } else {
+                newList.push({ id: product.id, quantity: 1 })
+            }
+            return {
+                ...state,
+                productsSelected: newList,
+            }
+        }),
+        updateQuantityProductSelected: (productId, quantity) => set((state) => {
+            const newList = [...state.productsSelected]
+            const indexItem = newList.findIndex(p => p.id === productId)
+            if (quantity <= 0)
+                newList.splice(indexItem, 1)
+            else
+                newList[indexItem].quantity = quantity
+            return {
+                ...state,
+                productsSelected: newList,
+            }
+        })
     }),
-    updateQuantityProductSelected: (productId, quantity) => set((state) => {
-        const newList = [...state.productsSelected]
-        const indexItem = newList.findIndex(p => p.id === productId)
-        if (quantity <= 0)
-            newList.splice(indexItem, 1)
-        else
-            newList[indexItem].quantity = quantity
-        return {
-            ...state,
-            productsSelected: newList,
-        }
-    })
-}))
-
+    {
+        name: 'product-storage', // name of the item in the storage (must be unique)
+        storage: createJSONStorage(() => AsyncStorage), 
+    }
+))
 export default useProductsStore
